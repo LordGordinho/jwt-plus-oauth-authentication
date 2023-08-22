2119d0a38772433c450b8519510cdbb8853876da
Jorge Felipe <jorgefelipess08@gmail.com>
2023-08-22 08:34:02 -0300

    add login with email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+         :rememberable, :validatable, authentication_keys: [:email_or_username]
+
+  attr_accessor :email_or_username
+
+  def email_or_username
+    @email_or_username || email || username
+  end
+
+  private
+
+  def self.find_for_database_authentication(condition)
+    find_by('lower(username) = :value OR lower(email) = :value', { value: condition[:email_or_username] })
+  end
 end
